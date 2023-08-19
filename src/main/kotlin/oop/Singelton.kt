cde1ecefb22a628af8f34db7317b713cda42289c
Royweru <royezi17@gmail.com>
2023-08-19 10:31:25 +0300

    Singleton object in kotlin

---
+++ src/main/kotlin/oop/Singelton.kt
@@ -0,0 +1,22 @@
+package oop
+
+fun main(args: Array<String>) {
+    val instance = Database.getInstance()
+    val instance2 = Database.getInstance()
+    println(instance)
+    println(instance2)
+}
+
+class Database private constructor(){
+  companion object{
+      private var instance:Database? = null
+      fun getInstance(): Database? {
+          if (instance == null){
+              instance = Database()
+          }
+              return instance
+
+      }
+  }
+
+}
